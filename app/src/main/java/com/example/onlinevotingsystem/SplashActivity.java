a7938dd0df9507e05c0f68f13fc36c5a8ae523ad
Banee Ishaque K <baneeishaque@gmail.com>
2021-04-11 12:40:12 +0530

    Typo in splash

--- app/src/main/java/com/example/onlinevotingsystem/SplashActivity.java
+++ app/src/main/java/com/example/onlinevotingsystem/SplashActivity.java
@@ -3,6 +3,8 @@ package com.example.onlinevotingsystem;
 import android.content.Intent;
 import android.os.Build;
 import android.os.Bundle;
+import android.content.Context;
+import android.util.Log;
 
 import androidx.annotation.RequiresApi;
 import androidx.appcompat.app.AppCompatActivity;
@@ -12,25 +14,34 @@ import java.util.Objects;
 
 public class SplashActivity extends AppCompatActivity {
 
-    @RequiresApi(api = Build.VERSION_CODES.LOLLIPOP)
+	Context activityContext = this;
+
+    // @RequiresApi(api = Build.VERSION_CODES.LOLLIPOP)
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+		
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splash);
 
-        getWindow().setStatusBarColor(ContextCompat.getColor(SplashActivity.this, R.color.colorAccent));
+        getWindow().setStatusBarColor(ContextCompat.getColor(activityContext, R.color.colorAccent));
 
         Objects.requireNonNull(getSupportActionBar()).hide();
 
         Thread thread = new Thread() {
-            public void run() {
-                try {
-                    sleep(4000);
-                } catch (Exception e) {
-                    e.printStackTrace();
-                } finally {
-                    Intent intent = new Intent(SplashActivity.this, MainActivity.class);
-                    startActivity(intent);
+        
+			public void run() {
+            
+				try {
+                
+					sleep(4000);
+                
+				} catch (Exception e) {
+					
+					Log.d(ApplicationSpecification.name, "Exception : " + e.toString());
+                
+				} finally {
+                
+                    startActivity(new Intent(activityContext, MainActivity.class));
                 }
             }
         };
